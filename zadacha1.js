8e45f07dfe541334debb49f9e70c41b02e0dba59
User Name <sh.pasha.v@gmail.com>
2020-02-17 13:21:01 +0300

    remove in folder

--- zadacha1.js
+++
@@ -1,7 +0,0 @@
-const a = Number(prompt("Чему равно a ?"));
-const b = Number(prompt("Чему равно b ?"));
-const c = Number(prompt("Чему равно c ?"));
-const d = Number(prompt("Чему равно d ?"));
-
-alert('Максимальное из этих чисел '+((a>d) ? ((a>b) ? ((a>c) ? a:c):((b>c) ? b:c)):((d>b) ? ((d>c) ? d:((c>b) ? c:b)):b)));
-alert ('A cреднее гармоническое этих чисел - ' + 4/(1/a+1/b+1/c+1/d))
